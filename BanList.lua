5afd1ef41a10d00809108d41b2c4472602d38a87
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-04-17 13:10:16 -0300

    Add files via upload

---
+++ BanList.lua
@@ -0,0 +1,16 @@
+local Ban = {
+	[1919390511] = {Motivo = "Wild fire all"},
+	[3380344108] = {Motivo = "Mutando todos do jogo e tirando patente"},
+	[3715134739] = {Motivo = "Unrank all, ban all"},
+	[2345451268] = {Motivo = "Acusando e tirando pat de todos falsamente."},
+	[4026779595] = {Motivo = "Banido."},
+	[1214755295] = {Motivo = "Spammando ref all."}, 
+	[3088948388] = {Motivo = "Tirando rank de geral, não volta mais."},
+	[3963271291] = {Motivo = "Gameban all, não volta mais."},
+	[2499057894] = {Motivo = "'eb lixo'"},
+	[4326175331] = {Motivo = "Joga na EB do P3U."},
+	[2521671238] = {Motivo = "Suspeita de infiltrado."},
+	[1795253070] = {Motivo = "Flw"},-- Derrubando a EB e dando rank all
+}
+
+return Ban 
