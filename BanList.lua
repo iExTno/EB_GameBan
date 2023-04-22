191d519d81eb752d60dbb0fee42a2e5d808fb784
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-04-22 12:18:12 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -15,7 +15,9 @@ local Ban = {
         [1449432756] = {Motivo = "chatice"},
 	[3651064753] = {Motivo = " "},-- Gameleaks
         [143220887] = {Motivo = "vendendo Ranks"},
-	[4343226099] = {Motivo = "pedido do ext"},-- Gameleaks
-	
+	[4343226099] = {Motivo = "pedido do ext"},
+	[4499219673] = {Motivo = "Abusando de comandos all."},
+	[840869435] = {Motivo = "Abusando de comandos all."},
+	[3673993643] = {Motivo = "Abusando de comandos all."},
 }
 return Ban 
