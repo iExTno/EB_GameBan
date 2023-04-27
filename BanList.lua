dff79c8050e4041e49ceca7d52509f10c23ad2ce
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-04-26 21:03:38 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -1,23 +1,18 @@
 local Ban = {
 	[1919390511] = {Motivo = "Wild fire all"},
-        [785068132] = {Motivo = "ref all"},
 	[3380344108] = {Motivo = "Mutando todos do jogo e tirando patente"},
 	[3715134739] = {Motivo = "Unrank all, ban all"},
 	[2345451268] = {Motivo = "Acusando e tirando pat de todos falsamente."},
 	[4026779595] = {Motivo = "Banido."},
 	[1214755295] = {Motivo = "Spammando ref all."}, 
-        [785068132] = {Motivo = "Spamando Ref all"},
 	[3088948388] = {Motivo = "Tirando rank de geral, não volta mais."},
 	[3963271291] = {Motivo = "Gameban all, não volta mais."},
 	[2499057894] = {Motivo = "'eb lixo'"},
-        [785068132] = {Motivo = "exploiter"},
 	[4326175331] = {Motivo = "Joga na EB do P3U."},
-        [785068132] = {Motivo = "conspirações"},
 	[2521671238] = {Motivo = "Suspeita de infiltrado."},
         [3000934539] = {Motivo = "Ameaçando"},
 	[1795253070] = {Motivo = "Flw"},-- Derrubando a EB e dando rank all
         [1449432756] = {Motivo = "chatice"},
-        [785068132] = {Motivo = "GameLeaks member"},
 	[3651064753] = {Motivo = " "},-- Gameleaks
 	[4343226099] = {Motivo = "pedido do ext"},
 	[4499219673] = {Motivo = "Abusando de comandos all."},
