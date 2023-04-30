ae2fbd817e96f8b19ec905df126ddfe8fda6fdba
iExTno <132175289+iExTno@users.noreply.github.com>
2023-04-30 17:59:13 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -12,7 +12,6 @@ local Ban = {
 	[2521671238] = {Motivo = "Suspeita de infiltrado."},
         [3000934539] = {Motivo = "Ameaçando"},
 	[1795253070] = {Motivo = "Flw"},-- Derrubando a EB e dando rank all
-        [1449432756] = {Motivo = "chatice"},
 	[3651064753] = {Motivo = " "},-- Gameleaks
 	[4343226099] = {Motivo = "pedido do ext"},
 	[4499219673] = {Motivo = "Abusando de comandos all."},
